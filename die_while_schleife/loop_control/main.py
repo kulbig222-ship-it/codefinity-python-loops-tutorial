ec23ace0f26fc17c641f1dc8dbc1ab6efe737201
kulbig222-ship-it <>
2026-05-26 14:21:28 +0000

    Loop Control

---
+++ die_while_schleife/loop_control/main.py
@@ -0,0 +1,19 @@
+# List of country names
+countries = ['Wales', 'Denmark', 'Belgium', 'Japan', 'South Korea', 'South Africa', 'Indonesia', 'Singapore', 'Australia', 'India', 'Saudi Arabia', 'Mexico', 'Turkey', 'Greece', 'Netherlands', 'Finland', 'Monako', 'United Arab Emirates', 'Egypt', 'Morocco', 'Brazil', 'Argentina', 'Ireland', 'Portugal', 'Chile', 'Spain', 'Czech Republic', 'Sweden', 'Switzerland', 'Thailand', 'Luxemburg', 'New Zealand', 'France', 'Italy', 'Germany', 'China', 'Canada', 'Hungary', 'Scotland', 'Norway', 'Austria', 'Ukraine', 'Poland']
+
+# List to hold selected countries
+selected = []
+
+# Iterate through countries using a while loop
+i = 0
+while i < len(countries):
+    if not countries[i].startswith('S'):
+        i += 1
+        continue
+    selected.append(countries[i])
+    if len(selected) == 3:
+        break
+    i += 1
+
+# Testing
+print('First three countries starting with "S":', selected)
